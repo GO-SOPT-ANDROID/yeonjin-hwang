1385970a1e185517e69a8bf1c598c8079368c252
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-05-13 01:06:22 +0900

    [Feature/#5] 회원가입, 로그인 서버 연결

--- app/src/main/java/org/android/go/sopt/presentation/auth/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/auth/LoginActivity.kt
@@ -8,19 +8,26 @@ import android.view.MotionEvent
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContentProviderCompat.requireContext
+import org.android.go.sopt.MainActivity
 import org.android.go.sopt.presentation.home.ProfileActivity
 import org.android.go.sopt.R
 import org.android.go.sopt.data.User
+import org.android.go.sopt.data.api.ServicePool
+import org.android.go.sopt.data.remote.model.RequestLoginDto
+import org.android.go.sopt.data.remote.model.ResponseLoginDto
+import org.android.go.sopt.data.remote.service.LoginService
 import org.android.go.sopt.databinding.ActivityLoginBinding
-import org.android.go.sopt.util.IntentKey
-import org.android.go.sopt.util.hideKeyboard
-import org.android.go.sopt.util.showToast
-import org.android.go.sopt.util.showSnackBar
+import org.android.go.sopt.util.*
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class LoginActivity : AppCompatActivity() {
 
     private var user: User? = null
     private lateinit var binding: ActivityLoginBinding
+    private var loginService = ServicePool.loginService
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +58,7 @@ class LoginActivity : AppCompatActivity() {
     private fun initLoginEvent() {
         binding.btnLogin.setOnClickListener {
             if (isCheckData()) {
-                setLoginData()
+                completeLogin()
             } else {
                 showSnackBar(getString(R.string.sign_in_fail), binding)
             }
@@ -80,6 +87,37 @@ class LoginActivity : AppCompatActivity() {
         } else {
             showSnackBar(getString(R.string.login_incorrect), binding)
         }
+    }
+
+    private fun completeLogin() {
+        loginService.login(
+            with(binding) {
+                RequestLoginDto(
+                    etId.text.toString(),
+                    etPw.text.toString()
+                )
+            }
+        ).enqueue(object : retrofit2.Callback<ResponseLoginDto> {
+            override fun onResponse(
+                call: Call<ResponseLoginDto>,
+                response: Response<ResponseLoginDto>
+            ) {
+                if (response.isSuccessful) {
+                    startActivity(
+                        Intent(this@LoginActivity, MainActivity::class.java)
+                    )
+                    response.body()?.message?.let { showToast(it) } ?: R.string.sign_in_done
+                    if (!isFinishing) finish()
+                }
+                else {
+                    response.body()?.message?.let { showToast(it) } ?: R.string.login_incorrect
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseLoginDto>, t: Throwable) {
+                t.message?.let { showToast(it) } ?: "서버통신 실패"
+            }
+        })
     }
 
     private fun isCorrect(): Boolean {
--- app/src/main/java/org/android/go/sopt/presentation/auth/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/auth/SignUpActivity.kt
@@ -6,13 +6,20 @@ import android.os.Bundle
 import android.view.MotionEvent
 import org.android.go.sopt.R
 import org.android.go.sopt.data.User
+import org.android.go.sopt.data.api.ServicePool
+import org.android.go.sopt.data.remote.model.RequestSignUpDto
+import org.android.go.sopt.data.remote.model.ResponseSignUpDto
 import org.android.go.sopt.databinding.ActivitySignUpBinding
 import org.android.go.sopt.util.IntentKey
 import org.android.go.sopt.util.hideKeyboard
 import org.android.go.sopt.util.showSnackBar
+import org.android.go.sopt.util.showToast
+import retrofit2.Call
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private val signUpService = ServicePool.signUpService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +32,7 @@ class SignUpActivity : AppCompatActivity() {
     private fun signUp() {
         binding.btnSignup.setOnClickListener {
             if (isCheckData() && isCheckLength()) {
-                setUser()
+                completeSignUp()
             } else {
                 showSnackBar(getString(R.string.sign_up_incorrect), binding)
             }
@@ -33,8 +40,8 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun isCheckData(): Boolean {
-        return binding.etId.text.isNotEmpty() && binding.etPw.text.isNotEmpty()
-                && binding.etName.text.isNotEmpty() && binding.etFeature.text.isNotEmpty()
+        return binding.etId.text.isNotBlank() && binding.etPw.text.isNotBlank()
+                && binding.etName.text.isNotBlank() && binding.etFeature.text.isNotBlank()
     }
 
     private fun isCheckLength(): Boolean {
@@ -54,6 +61,38 @@ class SignUpActivity : AppCompatActivity() {
         if (!isFinishing) {
             finish()
         }
+    }
+
+    private fun completeSignUp() {
+        signUpService.signUp(
+            with(binding) {
+                RequestSignUpDto(
+                    etId.text.toString(),
+                    etPw.text.toString(),
+                    etName.text.toString(),
+                    etFeature.text.toString()
+                )
+            }
+        ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
+            override fun onResponse(
+                call: Call<ResponseSignUpDto>,
+                response: Response<ResponseSignUpDto>
+            ) {
+                if (response.isSuccessful) {
+                    startActivity(
+                        Intent(this@SignUpActivity, LoginActivity::class.java)
+                    )
+                    response.body()?.message?.let { showToast(it) } ?: "회원가입 성공"
+                    if (!isFinishing) finish()
+                } else {
+                    response.body()?.message?.let { showToast(it) } ?: "서버통신 실패"
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                t.message?.let { showToast(it) } ?: "서버통신 살패"
+            }
+        })
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
